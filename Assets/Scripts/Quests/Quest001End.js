b1e748382d5b8b4b271f6a4aa4d54b003833ad5c
Uchida <Uchida@Uchida-no-MacBook-Pro.local>
2016-09-01 11:53:58 +0900

    初めてのコミットなう。

---
+++ Assets/Scripts/Quests/Quest001End.js
@@ -0,0 +1,45 @@
+﻿import UnityEngine.UI; // We need this to use the component in the TextBox
+
+var TextBoxOnCheck : int = 0; // Checks if TextBox is on the screen
+var MessageBox : GameObject; // Defines the message box as an object
+var TextBox : GameObject; // Defines the text box as an object
+var QuestBox : GameObject; // Defines the quest box
+var QuestText : GameObject; // Defines the text in the Quest box
+var QuestName : String; // the quest name we can change
+
+
+
+function OnMouseDown () {
+			if (TextBoxOnCheck == 0) {
+				TextBoxOnCheck = 1; //Sets the variable to turn the box off
+				MessageBox.SetActive(true); //Re-enables the box
+				TextBox.GetComponent.<Text>().text = "Villager: Thank you very much."; //changes the message
+				QuestText.GetComponent.<Text>().text = "Active Quest: None"; 
+			}
+				
+			else {
+				TextBoxOnCheck = 0;
+				MessageBox.SetActive(false);
+				//TextMessage = "Villager: Get going then!";"
+			}
+}
+
+function Update () {
+	if (Input.GetButtonDown("Submit")) {
+		if(TextBoxOnCheck == 1){
+			MessageBox.SetActive(false);
+			TextBoxOnCheck = 0;
+	}
+
+	}
+	if (Input.GetButtonDown("Cancel")) {
+		if(TextBoxOnCheck == 1){
+			MessageBox.SetActive(false);
+			TextBoxOnCheck = 0;
+		
+	}
+
+	}
+}
+
+
---
+++ Assets/Scripts/Quests/Quest001Start.js
@@ -0,0 +1,49 @@
+﻿import UnityEngine.UI; // We need this to use the component in the TextBox
+
+var TextBoxOnCheck : int = 0; // Checks if TextBox is on the screen
+var MessageBox : GameObject; // Defines the message box as an object
+var TextBox : GameObject; // Defines the text box as an object
+var TextMessage : String; // The text we can change
+var QuestBox : GameObject; // Defines the quest box
+var QuestText : GameObject; // Defines the text in the Quest box
+var QuestName : String; // the quest name we can change
+
+var QuestItemToShow : GameObject;
+
+function OnMouseDown () {
+			if (TextBoxOnCheck == 0) {
+			QuestItemToShow.SetActive(true);
+				TextBoxOnCheck = 1; //Sets the variable to turn the box off
+				MessageBox.SetActive(true); //Re-enables the box
+				TextBox.GetComponent.<Text>().text = TextMessage; //changes the message
+				QuestName = "Active Quest: 'お金はどこに落ちてる？'"; // change the quest name
+				QuestText.GetComponent.<Text>().text = QuestName; // input the quest name
+			}
+				
+			else {
+				TextBoxOnCheck = 0;
+				MessageBox.SetActive(false);
+				TextMessage = "内田：いい返事期待してるよ！";
+			}
+}
+
+function Update () {
+	if (Input.GetButtonDown("Submit")) {
+		if(TextBoxOnCheck == 1){
+			MessageBox.SetActive(false);
+			TextBoxOnCheck = 0;
+			TextMessage = "内田：いい返事期待してるよ！";
+	}
+
+	}
+	if (Input.GetButtonDown("Cancel")) {
+		if(TextBoxOnCheck == 1){
+			MessageBox.SetActive(false);
+			TextBoxOnCheck = 0;
+			TextMessage = "内田：いい返事期待してるよ！";
+	}
+
+	}
+}
+
+
